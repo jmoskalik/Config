e0c7378b61259dde5f55d8c66a750bf4dd32b47a
XaBix <118305310+xabix99@users.noreply.github.com>
2023-09-15 23:34:46 +0200

    Update script.sh

--- UbuntuVM/script.sh
+++ UbuntuVM/script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install vim gpg git neofetch dconf gnome-shell-extensions gnome-tweaks python3 -y &&
+sudo apt-get install vim gpg git neofetch dconf gnome-shell-extensions gnome-tweaks python3 papirus-icon-theme -y &&
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -9,6 +9,7 @@ sudo apt install apt-transport-https &&
 sudo apt update &&
 sudo apt install code &&
 gnome-terminal -- code . &&
+gnome-terminal -- gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
 gnome-terminal -- gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' &&
 gnome-terminal -- gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false &&
 gnome-terminal -- gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM &&

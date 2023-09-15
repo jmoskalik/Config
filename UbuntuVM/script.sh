940ade956e90a2cfb7fe7d05d972e099862d35b8
XaBix <118305310+xabix99@users.noreply.github.com>
2023-09-16 00:27:24 +0200

    Update script.sh

--- UbuntuVM/script.sh
+++ UbuntuVM/script.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-sudo apt-get install vim gpg git neofetch dconf gnome-shell-extensions gnome-tweaks python3 -y &&
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg &&
-sudo apt install apt-transport-https &&
-sudo apt update &&
+sudo apt-get install vim gpg git neofetch dconf gnome-shell-extensions software-properties-common apt-transport-https gnome-tweaks python3 -y &&
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add - &&
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" &&
 sudo apt install code &&
 gnome-terminal -- code . &&
 gnome-terminal -- gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' &&
